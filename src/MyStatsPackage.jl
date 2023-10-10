eb8734f798d5fd6497a31c73f8ffb8d16ca9da5c
Torsten Giess <torsten.giess@ibtb.uni-stuttgart.de>
2023-10-10 10:14:47 +0200

    Update MyStatsPackage.jl

--- src/MyStatsPackage.jl
+++ src/MyStatsPackage.jl
@@ -5,4 +5,8 @@ include("./statistic_functions.jl")
 export rse_sum, rse_mean, rse_std, rse_tstat
 export StatResult
 
+function printOwner()
+    print("torogi94")
+end
+
 end # module MyStatsPackage
