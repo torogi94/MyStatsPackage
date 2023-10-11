2e4644045bfff5c690bccebb6e22bb9aec635294
Torsten Giess <torsten.giess@ibtb.uni-stuttgart.de>
2023-10-11 09:46:16 +0200

    Add docstrings

--- src/statistic_functions.jl
+++ src/statistic_functions.jl
@@ -2,43 +2,135 @@
 using Test
 
 #---
-function rse_sum(args)
+"""
+    rse_sum(x::Vector)::Real
+
+Return the sum of elements in a Vector.
+
+# Arguments
+
+- `x` -- Vector
+
+# Output
+
+A real number representing the sum of elements.
+
+# Notes
+
+I mean, it's a sum. Not rocket science.
+
+# Examples
+
+```julia-repl
+julia> rse_sum(1:3)
+6
+\```
+
+See also [`rse_mean`](@rse_mean), [`rse_std`](@rse_std), [`rse_tstat`](@rse_tstat).
+"""
+function rse_sum(x)
     sum = 0
-    for arg = args
+    for arg = x
         sum += arg
     end
     return sum
 end
 
 #---
-@test rse_sum(1:36) == 666
+"""
+    rse_mean(x::Vector)::Real
 
-#---
-function rse_mean(args)
-    return rse_sum(args) / length(args)
+Return the mean of elements in a Vector.
+
+# Arguments
+
+- `x` -- Vector
+
+# Output
+
+A real number representing the mean of elements.
+
+# Notes
+
+I mean, it's a mean. Not rocket science.
+
+# Examples
+
+```julia-repl
+julia> rse_mean(1:3)
+2
+\```
+
+See also [`rse_sum`](@rse_sum), [`rse_std`](@rse_std), [`rse_tstat`](@rse_tstat).
+"""
+function rse_mean(x)
+    return rse_sum(x) / length(x)
 end
 
 #---
-@test rse_mean(-15:17) == 1
+"""
+    rse_std(x::Vector)::Real
 
-#---
-function rse_std(args)
-    return sqrt(rse_sum((args .- rse_mean(args)) .^ 2) / (length(args) - 1))
+Return the standard deviation of elements in a Vector.
+
+# Arguments
+
+- `x` -- Vector
+
+# Output
+
+A real number representing the standard deviation of elements.
+
+# Notes
+
+Calculates by the following formula: \sqrt{\frac{\sum((x-mean(x))^2)}{n-1}}
+
+# Examples
+
+```julia-repl
+julia> rse_std(1:3)
+1
+\```
+
+See also [`rse_sum`](@rse_sum), [`rse_mean`](@rse_mean), [`rse_tstat`](@rse_tstat).
+"""
+function rse_std(x)
+    return sqrt(rse_sum((x .- rse_mean(x)) .^ 2) / (length(x) - 1))
 end
 
 #---
-@test rse_std(1:3) == 1
+"""
+    rse_tstat(x::Vector,[σ]::Real=nothing)::Real
 
-#---
-function rse_tstat(args; σ=nothing)
+Return the t-value of elements in a Vector.
+
+# Arguments
+
+- `x` -- Vector
+
+# Output
+
+A real number representing the t-value of elements.
+
+# Notes
+
+Calculates by the following formula: \frac{mean(x)}{std(x) / (sqrt(length(x)))}
+
+# Examples
+
+```julia-repl
+julia> rse_tstat(2:3)
+5
+\```
+
+See also [`rse_sum`](@rse_sum), [`rse_mean`](@rse_mean), [`rse_std`](@rse_std).
+"""
+function rse_tstat(x; σ=nothing)
     if σ != isnothing
-        σ = rse_std(args)
+        σ = rse_std(x)
     end
-    return rse_mean(args) ./ (σ ./ sqrt.(length(args)))
+    return rse_mean(x) ./ (σ ./ sqrt.(length(x)))
 end
-
-#---
-@test rse_tstat(2:3) == 5.0
 
 #---
 struct StatResult
